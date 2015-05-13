771c8e10ee3b0007beb395b70271326ecacfee49
Shilon <shilong_92@hotmail.com>
2015-05-13 14:00:02 -0500

    ultimos cambios en la vista para imprimir

--- ProyectoAv/app/views/emails/formatestimation.blade.php
+++ ProyectoAv/app/views/emails/formatestimation.blade.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 	<title>Presupuesto</title>
 	<style type="text/css">
 		body{
 			font-size: 13px;
 		}
-		table{
-			border:1px solid #CCC;
-			border-radius: 50px;
+		table{			
 			width: 100%;			
 		}
 		.nums{					
@@ -31,10 +30,12 @@
 			padding: 0 5px;		
 		}	
 		.table-data{
-			margin-top: 10px;
-			border: 1px solid #CCC;			
-		}		
+			margin-top: 10px;			
+		}	
+		.table-data td{			
+		}
 		.table-articles{
+			border: 1px solid #CCC; 
 			text-align: center;						
 			margin-top: 10px;			
 		}
@@ -77,15 +78,28 @@
 			color: red;
 			font-size: 16px;		
 		}
+		.border{
+			border:1px solid #CCC;
+		}
 		footer{
 			font-size: 12px;
 			margin-top: 10px;
+		}
+		.header{
+			padding-left: 10px;
+		}		
+		.header td p,div{
+			margin: 0;
+			padding: 0;
+		}
+		.top-data{
+			margin-bottom: 10px;
 		}		
 		
 	</style>
 </head>
 <body>		
-	<table style="border-radius:10px">
+	<table class="header">
 		<tr>
 			<td width="100px">
 				<img src="images/logoav.png" height="90px" width="90px">			
@@ -95,7 +109,7 @@
 				<p>RENTA Y BANQUETES</p>
 			</td>
 			<td>
-				<span class="text-center">Fecha {{$estimation->created_at}}</span>
+				<p class="text-center" style="margin-bottom:15px; background:1px solid #EEE">Fecha {{$estimation->created_at}}</p>
 				<div class="nums">
 					<span class="capital">61</span>
 					<div class="phones">
@@ -105,36 +119,50 @@
 					</div>
 				</div>				
 			</td>
-			<td>
-				<p>folio</p>
-				<p>datos de la empresa</p>
+			<td class="text-center">
+				<p style="margin-bottom:10px; background:1px solid #EEE">00000{{$estimation->id}}</p>
+				<div>
+					<p>3A. SUR PONIENTE No. 1041</p>					
+					<p>TUXTLA GUTIERREZ CHIAPAS</p>
+					<p>grupoav@hotmail.com</p>
+				</div>
 			</td>
 		</tr>		
 	</table>
-	<table class="table-data">
+	<table class="table-data border">
 		<tr>
-			<td width="59%" colspan="2">NOMBRE <span> {{$estimation->costumer_name}}</span></td>			
-			<td width="40%" colspan="2">FECHA DEL EVENTO <span> {{$estimation->date_event}}</span></td>			
+			<td>NOMBRE</td>			
+			<td colspan="3"> <span>{{$estimation->costumer_name}}</span></td>
+			<td>FECHA DEL EVENTO</td>
+			<td colspan="3"><span>{{$estimation->date_event}}</span></td>
 		</tr>
 		<tr>
-			<td width="99%" colspan="4">DOM. DEL EVENTO <span>{{$estimation->event_address}}</span></td>
+			<td colspan="2" width="100px">DOM. DEL EVENTO</td>
+			<td colspan="4"><span>{{$estimation->event_address}}</span></td>
 		</tr>
 		<tr>
-			<td width="99%" colspan="4">DOM. DEL PARTICULAR <span>{{$estimation->home_address}}</span></td>			
+			<td colspan="2">DOM. PARTICULAR</td>
+			 <td colspan="2"><span>{{$estimation->home_address}}</span></td>
+			<td>RECOGER</td>			
+			<td colspan="2"><span>{{$estimation->date_collecting}}</span></td>
 		</tr>
 		<tr>
-			<td width="10%">TEL <span>{{$estimation->phone}}</span></td>
-			<td width="40%">EMAIL <span>{{$estimation->email}}</span></td>
-			<td width="25%">SURTIR <span>{{$estimation->date_range}}</span></td>
-			<td width="25%">RECOGER <span>{{$estimation->date_collecting}}</span></td>
+			<td>TEL</td>
+			<td><span>{{$estimation->phone}}</span></td>
+			<td>EMAIL</td>
+			<td><span>{{$estimation->email}}</span></td>
+			<td>SURTIR</td>			
+			<td colspan="2"><span>{{$estimation->date_range}}</span></td>
 		</tr>	
 		<tr>
-			<td width="18%">TIPO DE EVENTO<span>{{$estimation->type}}</span></td>
-			<td width="20%">No. DE PERSONAS<span>{{$estimation->number_people}}</span></td>
-			<td width="30%">COLOR<span>{{$estimation->color}}</span></td>
+			<td colspan="2">TIPO DE EVENTO</td>
+			<td><span>{{$estimation->type}}</span></td>
+			<td colspan="2">No. DE PERSONAS<span>{{$estimation->number_people}}</span></td>			
+			<td>COLOR</td>
+			<td><span>{{$estimation->color}}</span></td>
 		</tr>	
 	</table>
-	<table class="table table-articles">
+	<table class="table table-articles border">
 		<tr>
 			<td></td>
 			<td>ARTICULOS</td>
@@ -148,12 +176,12 @@
 			</tr>
 		@endforeach
 	</table>	
-	<table class="message text-center">
+	<table class="message text-center border">
 		<tr>
 			<td colspan="3">VIDEO FILMACIONES: 961 2629 030 OFIC.: 613-86-79</td>
 		</tr>
 	</table>	
-	<table class="table-section">
+	<table class="table-section border">
 		<tr>
 			<td rowspan="5" width="57%" class="text-center">
 				<p>EL EQUIPO SE RENTA POR DIA EVITE RECARGOS</p>
@@ -184,7 +212,7 @@
 			<td>{{number_format($estimation->balance,2,'.',',')}}</td>
 		</tr>				
 	</table>	
-	<table>
+	<table class="border" style="border-top: 1px solid #FFF;padding:7px 5px;">
 		<tr>
 			<td>
 				FAVOR DE REVISAR EL MOBILIARIO Y EQUIPO ESTEN COMPLETOS "NO CONFIE".
