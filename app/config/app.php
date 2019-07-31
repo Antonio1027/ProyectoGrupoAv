612cf579818a52514c606dc5996d58e44df4d527
antonio <jesus.shilon@enova.mx>
2019-07-30 19:13:38 -0500

    feature #9 Laravel generator deteled

--- app/config/app.php
+++ app/config/app.php
@@ -120,7 +120,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
 		'Thujohn\Pdf\PdfServiceProvider',
 		'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
 		'Barryvdh\Cors\CorsServiceProvider'
